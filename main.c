7b49f3b6c625b7a5c5ab36b2e08dac0d03c61994
derselbst <tom.mbrt@googlemail.com>
2014-12-18 19:46:33 +0100

    format main.c

--- main.c
+++ main.c
@@ -78,8 +78,8 @@ void usage(char filename[])
         "\t \t%s\t\t on-the-fly playback, you might hear some interrupts\n"
 #endif // PLAYBACK_SUPPORT
         "\t \t%s\t\t\t use high level audio emulation, will speed up emulation, at the expense of accuracy, and potential emulation bugs. \n"
-	"\t \t%s\t\t play forever\n"
-	"\t \t%s\t\t double the playing length read from usf\n"
+        "\t \t%s\t\t play forever\n"
+        "\t \t%s\t\t double the playing length read from usf\n"
         "\t \t%s\t\t use interpreter, slows down emulation; use it if recompiler (default) fails\n\n",
         filename,
         RoundFrequ,
@@ -110,117 +110,115 @@ int main(int argc, char** argv)
 
     if(usf_init(argv[1]))
     {
-      strcpy(filename, argv[1]);
+        strcpy(filename, argv[1]);
 
-	uint8_t i;
-      for (i = 2; i < argc; i++)
-      {
-	  if (((strcmp(argv[i],"-h"))==0) || ((strcmp(argv[i],"--help"))==0) || ((strcmp(argv[i],"--usage"))==0))
-	  {
-	      usage(argv[0]);
-	      return 0;
-	  }
-	  else if (((strcmp(argv[i],FadeType_LONG))==0) || ((strcmp(argv[i],FadeType))==0))
-	  {
-	      if (argv[i+1]!=NULL)
-	      {
-		  fade_type=atoi(argv[++i]);
-	      }
-	  }
-	  else if (((strcmp(argv[i],RoundFrequ_LONG))==0) || ((strcmp(argv[i],RoundFrequ))==0))
-	  {
-	      round_frequency=1;
-	  }
-  #ifdef FLAC_SUPPORT
-	  else if (((strcmp(argv[i],toFLAC))==0))
-	  {
-	      useFlac=1;
-	  }
-  #endif // FLAC_SUPPORT
-  #ifdef PLAYBACK_SUPPORT
-	  else if (((strcmp(argv[i],playback))==0))
-	  {
-	      playingback=1;
-	  }
-  #endif // PLAYBACK_SUPPORT
-	  else if (((strcmp(argv[i],useAudioHle))==0))
-	  {
-	      use_audiohle=1;
-	  }
-	  else if (((strcmp(argv[i],useInterpreterCPU))==0))
-	  {
-	      CPU_Type = CPU_Interpreter;
-	      RSP_Cpu = CPU_Interpreter;
-	  }
-	  else if (((strcmp(argv[i],forever))==0))
-	  {
-	      track_time |= 1 << (sizeof(uint32_t)*8 -1);
-	  }
-	  else if (((strcmp(argv[i],doubleLen))==0))
-	  {
-	      track_time *= 2;
-	  }
-      else if (((strcmp(argv[i],outFileNameFormatParam))==0))
-	  {
-	      //TODO: preserve path to file!!!
-
-if(!argv[++i])
-    continue;
-
-            memset(filename,'\0', sizeof(filename));
-	         strcpy(filename, argv[i+1]);
-
-    struct asdf
-    {
-        char wildcard[12];
-        char * replacement;
-    };
-
-    struct asdf wildcards[6]=  {
-                                {"%game%", game},
-                                {"%genre%", genre},
-                                {"%title%", title},
-                                {"%artist%", artist},
-                                {"%copyright%", copyright},
-                                {"%year%", year}
-                                };
-
-    unsigned short i;
-    for(i = 0; i<6; i++)
-    {
-        // find placeholder
-        char * pch = strstr (filename, wildcards[i].wildcard);
-        if(pch)
+        uint8_t i;
+        for (i = 2; i < argc; i++)
         {
-            size_t lenrepl = strlen(wildcards[i].replacement);
-            size_t lenwildc= strlen(wildcards[i].wildcard);
+            if (((strcmp(argv[i],"-h"))==0) || ((strcmp(argv[i],"--help"))==0) || ((strcmp(argv[i],"--usage"))==0))
+            {
+                usage(argv[0]);
+                return 0;
+            }
+            else if (((strcmp(argv[i],FadeType_LONG))==0) || ((strcmp(argv[i],FadeType))==0))
+            {
+                if (argv[i+1]!=NULL)
+                {
+                    fade_type=atoi(argv[++i]);
+                }
+            }
+            else if (((strcmp(argv[i],RoundFrequ_LONG))==0) || ((strcmp(argv[i],RoundFrequ))==0))
+            {
+                round_frequency=1;
+            }
+#ifdef FLAC_SUPPORT
+            else if (((strcmp(argv[i],toFLAC))==0))
+            {
+                useFlac=1;
+            }
+#endif // FLAC_SUPPORT
+#ifdef PLAYBACK_SUPPORT
+            else if (((strcmp(argv[i],playback))==0))
+            {
+                playingback=1;
+            }
+#endif // PLAYBACK_SUPPORT
+            else if (((strcmp(argv[i],useAudioHle))==0))
+            {
+                use_audiohle=1;
+            }
+            else if (((strcmp(argv[i],useInterpreterCPU))==0))
+            {
+                CPU_Type = CPU_Interpreter;
+                RSP_Cpu = CPU_Interpreter;
+            }
+            else if (((strcmp(argv[i],forever))==0))
+            {
+                track_time |= 1 << (sizeof(uint32_t)*8 -1);
+            }
+            else if (((strcmp(argv[i],doubleLen))==0))
+            {
+                track_time *= 2;
+            }
+            else if (((strcmp(argv[i],outFileNameFormatParam))==0))
+            {
+                if(!argv[++i])
+                {
+                    continue;
+                }
 
-                char buf[512];
-            strcpy(buf, pch+lenwildc);
-                strncpy(pch, wildcards[i].replacement, lenrepl);
+                //TODO: preserve path to file!!!
+                memset(filename,'\0', sizeof(filename));
+                strcpy(filename, argv[i+1]);
 
-            memset(pch+lenrepl,'\0', sizeof(filename)-lenrepl);
-                strcat(pch,buf);
+                struct
+                {
+                    char wildcard[12];
+                    char * replacement;
+                } wildcards[6] =
+                {
+                    {"%game%", game},
+                    {"%genre%", genre},
+                    {"%title%", title},
+                    {"%artist%", artist},
+                    {"%copyright%", copyright},
+                    {"%year%", year}
+                };
+
+                unsigned short i;
+                for(i = 0; i<6; i++)
+                {
+                    // find placeholder
+                    char * pch = strstr (filename, wildcards[i].wildcard);
+                    if(pch)
+                    {
+                        size_t lenrepl = strlen(wildcards[i].replacement);
+                        size_t lenwildc= strlen(wildcards[i].wildcard);
+
+                        char buf[512];
+                        strcpy(buf, pch+lenwildc);
+                        strncpy(pch, wildcards[i].replacement, lenrepl);
+
+                        memset(pch+lenrepl,'\0', sizeof(filename)-lenrepl);
+                        strcat(pch,buf);
+                    }
+                }
+//                puts(filename);
+            }
+            else
+            {
+                printf("Warning: Unknown commandline option %s\n", argv[i]);
+            }
         }
-    }
 
-    puts(filename);
-
-	  }
-	  else
-	  {
-	      printf("Warning: Unknown commandline option %s\n", argv[i]);
-	  }
-      }
-
-      if(useFlac)
-      {
-	  strcat(filename,".flac");
-      }
-      else
-      {
-	  strcat(filename,".au");
-      }
+        if(useFlac)
+        {
+            strcat(filename,".flac");
+        }
+        else
+        {
+            strcat(filename,".au");
+        }
         if(!usf_play())
         {
             printf("An Error occured while play.\n");
