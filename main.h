9e8851df5436b6737b8847221209edb294633da8
derselbst <tom.mbrt@googlemail.com>
2015-02-11 19:28:59 +0100

    fixed a bug when formatting outfilename

--- main.h
+++ main.h
@@ -1,9 +1,10 @@
 #include <stdint.h>
+#include <linux/limits.h>
 
 #define CPU_Interpreter             1
 #define CPU_Recompiler              0
 
-extern char filename[512];
+extern char filename[PATH_MAX];
 
 void DisplayError (char * Message, ...);
 void StopEmulation(void);
