e5315e88eb5a19b9941089cdad68bce9af85c5de
derselbst <tom.mbrt@googlemail.com>
2015-02-14 09:47:23 +0100

    fix build

--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #define CPU_Interpreter             1
 #define CPU_Recompiler              0
 
-extern char filename[PATH_MAX];
+extern char filename[PATH_MAX+1];
 
 void DisplayError (char * Message, ...);
 void StopEmulation(void);
